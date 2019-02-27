fea4328356e432167310f3e37257aca4c9b4a2a0
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-27 21:43:52 +0000

    Added base routes and view functions

--- processor/urls.py
+++ processor/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="index")
+    path('', views.overview_week,name="week"),
+    path('trend/', views.overview_trend,name="trend"),
+    path('farms/', views.farms_all,name="farms"),
+    path('reports/', views.farms_report,name="reports"),
+    path('workers/', views.farms_workers,name="workers"),
+    path('requests/', views.farms_request,name="requests"),
+    path('farms/<int:id>', views.single_farm,name="farm"),
+    path('workers/<int:id>', views.single_worker,name="worker"),
+    path('reports/<int:id>', views.single_report,name="report")
 ]
 
 
--- processor/views.py
+++ processor/views.py
@@ -3,6 +3,39 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World")
+def overview_week(request):
+    pass
+
+
+def overview_trend(request):
+    pass
+
+
+def farms_all(request):
+    pass
+
+
+def farms_report(request):
+    pass
+
+
+def farms_workers(request):
+    pass
+
+
+def farms_request(request):
+    pass 
+
+
+def single_farm(request,id):
+    pass
+
+
+def single_worker(request,id):
+    pass
+
+
+def single_report(request,id):
+    pass
+
 
