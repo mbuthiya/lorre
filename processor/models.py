5c30cf70111fecd1345f15b2f2f53fa3a9fb51a6
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-28 21:37:13 +0000

    Add Extension worker model

--- processor/models.py
+++ processor/models.py
@@ -4,9 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Processor(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    country = models.CharField(max_length = 50,null=True )
-    primary_product = models.OneToOneField(
-        Crop, on_delete=models.CASCADE, null=True)
+    country = models.CharField(max_length = 50,null=True)
+    primary_product = models.CharField(
+        Crop, null=True)
     unit_of_measure = models.CharField(max_length=10, null=True)
     company_image = models.ImageField(
-        upload_to="Processor_Profiles", null=True)
+        upload_to="processor_profiles", null=True)
+
+
+class ExtensionWorker(models.Model):
+    processor = models.ForeignKey(Processor,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    profile_image = models.ImageField(upload_to="worker_images")
+    phone_number = models.IntegerField(max_length=10)
+    started_work = models.DateField()
+    gender=models.CharField(max_length=8)
+
+
