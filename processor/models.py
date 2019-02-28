9c3d85186e0e5db47c95fd8de531519b2ca6a876
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-28 21:16:10 +0000

    Add the processor model

--- processor/models.py
+++ processor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Processor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    country = models.CharField(max_length = 50,null=True )
+    primary_product = models.OneToOneField(
+        Crop, on_delete=models.CASCADE, null=True)
+    unit_of_measure = models.CharField(max_length=10, null=True)
+    company_image = models.ImageField(
+        upload_to="Processor_Profiles", null=True)
