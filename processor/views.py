46408c9401f5c6b2ed2e1ccab1a878ad6de82482
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-28 08:48:59 +0000

    Add signup and login templates
    Created the signup and login templates
    Created the login and signup forms

--- processor/views.py
+++ processor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
+
 # Create your views here.
 
 def overview_week(request):
@@ -54,6 +55,6 @@ def signup(request):
         print(newUser.id)
         newUser.save()
 
-    
+    return render(request,"auth-templates/login")
 
         
