d73e8146e4047165b5ce51c8cd41c7023246a55d
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-03-05 22:21:30 +0000

    Heroku deploy

---
+++ static/js/farm.js
@@ -0,0 +1,9 @@
+$(".farm-dets").click(function(){
+    $("#farm-details-menu-section").show()
+    $("#farm-report-menu-section").hide()
+})
+
+$(".farm-reps").click(function(){
+    $("#farm-details-menu-section").hide()
+    $("#farm-report-menu-section").show()
+})
